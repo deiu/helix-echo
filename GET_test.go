8d49a3981e899c3c8293c443baf7974d2b2b3f0f
deiu <andrei@fcns.eu>
2016-10-27 16:54:59 -0400

    adds first benchmark

--- GET_test.go
+++ GET_test.go
@@ -1,6 +1,7 @@
 package helix
 
 import (
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"testing"
@@ -16,4 +17,18 @@ func Test_GET_Request(t *testing.T) {
 	body, err := ioutil.ReadAll(res.Body)
 	res.Body.Close()
 	assert.Equal(t, "Hello world", string(body))
+}
+
+func BenchmarkGET(b *testing.B) {
+	e := 0
+	for i := 0; i < b.N; i++ {
+		req, _ := http.NewRequest("GET", testServer.URL, nil)
+		if res, _ := testClient.Do(req); res.StatusCode != 200 {
+			e++
+		}
+	}
+	if e > 0 {
+		b.Log(fmt.Sprintf("%d/%d failed", e, b.N))
+		b.Fail()
+	}
 }
